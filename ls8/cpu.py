eb3b20f14e63e8728f3a15013e2a974898fa823b
Tim Griffith <grifmang@gmail.com>
2020-05-19 18:54:08 -0400

    saving code, gettin there

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -23,34 +23,44 @@ class CPU:
     def print_stuff(self, address):
         print(f'Value: {self.ram_read(address)}')
 
-    def load(self):
+    def mult(self,address_one, address_two):
+        print(address_one)
+        print(address_two)
+        print(address_one * address_two)
+
+    def load(self, file):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        with open(file) as program:
+            for instruction in program:
+                try:
+                    instruction = int(instruction.split('#')[0][:-1], 2)
+                    self.ram[address] = instruction
+                    address += 1
+                except:
+                    continue
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc *TODO day_two?*
+        #elif op == "SUB": etc
+        elif op == 'MUL':
+            pass
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -78,17 +88,26 @@ class CPU:
         """Run the CPU."""
         count = 0
         while not self.halted:
-            print(count)
-            self.load()
+            # print(count)
+            # self.load()
             instruction = self.ram[self.pc]
             if instruction == 1:
                 self.halt()
             elif instruction == 71:
                 self.print_stuff(self.ram[self.pc + 1])
                 self.pc += 2
+                # self.pc += instruction >> 8
             elif instruction == 130:
                 self.ram_write(self.ram[self.pc + 1], self.ram[self.pc + 2])
                 self.pc += 3
+                # self.pc += instruction >> 8
+            elif instruction == 162:
+                print(self.ram)
+                self.mult(self.ram[self.pc + 1], self.ram[self.pc + 2])
+                mask = 0b11000000 & instruction
+                print('mask', mask)
+                # self.pc += instruction >> 8
+                self.pc += 2
             else:
                 print(f'unknown instruction {instruction} at address {self.pc}')
                 break
